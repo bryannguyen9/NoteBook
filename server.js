981b6edca911c0bb5cf245f5226e4c41804687cf
bryannguyen9 <127802656+bryannguyen9@users.noreply.github.com>
2023-04-25 19:26:22 -0700

    server.js instantiates app and port and parses json data, also .use for different routes that have not been worked on yet, and .listen for port, might need to include more requires for reading/writing

---
+++ server.js
@@ -0,0 +1,23 @@
+// require dependencies
+const express = require('express');
+// might need fs, util, read and write rquires here
+
+// initalize express
+const app = express();
+
+// create port
+const PORT = 3001;
+
+// Middleware for parsing JSON data
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+// import routes for getting and posting data
+const apiRoutes = require('./routes/apiRoutes.js');
+app.use(apiRoutes);
+const htmlRoutes = require('./router/htmlRoutes.js');
+app.use(htmlRoutes);
+
+// server listener for connections
+app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));
